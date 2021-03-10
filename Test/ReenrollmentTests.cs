9fef0d62a999d773f7ffc25b1aefe3902c6f6f76
JoeKF <jvanwanzeele@keyfactor.com>
2021-03-10 17:00:16 -0500

    added license file and headers

--- Test/ReenrollmentTests.cs
+++ Test/ReenrollmentTests.cs
@@ -1,7 +1,12 @@
-using System;
+// Copyright 2021 Keyfactor
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions
+// and limitations under the License.
+
 using FluentAssertions;
 using Keyfactor.Integrations.Orchestrator.Template.Jobs;
-using Keyfactor.Integrations.Orchestrator.Test;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Mocks = Keyfactor.Integrations.Orchestrator.Test.Mocks;
@@ -28,8 +33,7 @@ namespace Keyfactor.OrchestratorTemplate.Test
 
             // Mock and configure helper method or client class to throw an exception or return a bad result.
             // example:
-
-
+            
             //config.Store.Properties = JsonConvert.SerializeObject(new
             //{
             //    VaultUrl = "https://test.vault",
